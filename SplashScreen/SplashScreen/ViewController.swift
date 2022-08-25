73ba6670e223d2ee045f1a2489ee09c7858dd7c3
AnjaliBE <105102832+AnjaliBE@users.noreply.github.com>
2022-08-25 14:02:29 +0530

    Add files via upload

---
+++ SplashScreen/SplashScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplashScreen
+//
+//  Created by Mac on 14/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
